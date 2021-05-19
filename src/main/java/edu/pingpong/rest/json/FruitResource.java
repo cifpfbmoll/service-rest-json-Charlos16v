efddcb5a3e34095fcdee882a15b7aab9388bea4f
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-05-19 21:37:55 +0200

    refactor(FruitResource): retrieving data from service layer and encapsulated in a response with ok status code

--- src/main/java/edu/pingpong/rest/json/FruitResource.java
+++ src/main/java/edu/pingpong/rest/json/FruitResource.java
@@ -1,16 +1,35 @@
 package edu.pingpong.rest.json;
 
+import org.jboss.logging.annotations.Pos;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Set;
 
 @Path("/fruits")
 public class FruitResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+   @Inject
+   FruitService service;
+
+    public FruitResource() {
     }
+
+    @GET
+    public Response fruitsData() {
+        return Response.ok(service.getData()).build();
+    }
+    /*
+    @POST
+    public Set<Fruit> add(Fruit fruit) {
+        fruitSet.add(fruit);
+        return fruitSet;
+    }*/
 }
